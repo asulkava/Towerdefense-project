fc63fa9ed8c6ad39d6bf09f2251f8a394a59a291
asulk <asulk@DESKTOP-QIN5D9B>
2020-04-17 18:39:14 +0300

    added a map loader, which loads the map files.

---
+++ src/Main/MapLoader.scala
@@ -0,0 +1,124 @@
+package Main
+
+import scala.collection.mutable.Buffer 
+import processing.core._
+import scala.io._
+import java.io._
+import Interface.loader
+import Main._
+
+
+object Path { // Object for the path
+  
+  var points = scala.collection.mutable.Buffer[Pos]()
+  
+}
+
+
+class MapTile(image: String, var x: Int, var y: Int, var solid: Boolean) {
+  
+  var img = loader.get(image).get
+  var location = Pos(x*32, y*32)
+  
+  def draw() = {
+    main.image(img, x*32, y*32, 32, 32)
+  }
+ 
+  
+}
+
+object GameMap {
+  val width = 25
+  val height = 19
+  
+  val grid = Array.ofDim[MapTile](width, height)
+  val filename = new File("./resources/mapFiles")
+  val fileList = filename.list()
+  
+  var generatedMap : PImage = _
+  
+  var maps: Map[String, Buffer[String]] = Map() 
+  
+  var paths: Map[String, Buffer[Pos]] = Map()
+  
+  var mapTiles = Buffer[String]() 
+  var spawnerPos = Buffer[Pos]()
+  var endPos = Buffer[Pos]()
+  
+
+  def loadMaps() = {
+    try {
+      for (file <- fileList) {
+        var tempMap: Buffer[String] = Buffer()
+        var tempPath: Buffer[Pos] = Buffer()
+        
+        for(line <- scala.io.Source.fromFile("resources/mapFiles/" + file).getLines()) {
+          
+          if (line(0) == '#') {
+            var pathPos = line.drop(1).split(",")
+            tempPath += Pos(pathPos(0).toDouble, pathPos(1).toDouble)
+          }
+          else {
+            tempMap += line
+          }
+        }
+        maps = maps + (file -> tempMap)            
+        paths = paths + (file -> tempPath)
+      }
+    } 
+    catch {
+      case ex: Exception => println("Error with map file.")
+    }
+    
+    mapTiles = maps.values.head
+    Path.points = paths.values.head
+  }
+  
+
+  def drawGround() = {
+    for(x <- 0 until width; y <- 0 until height) {
+      if(!grid(x)(y).solid)
+      grid(x)(y).draw
+    }
+  }
+
+  
+  def drawWalls() = {
+    for(x <- 0 until width; y <- 0 until height) {
+      if(grid(x)(y).solid) {
+        grid(x)(y).draw
+      }
+    }
+  }
+  
+
+  def getTile(x: Double, y: Double) : MapTile = {
+    var x_pos = (x/32).toInt
+    var y_pos = (y/32).toInt
+    
+    grid(x_pos)(y_pos)
+  }
+
+  
+  def getTile(pos: Pos) : MapTile = {
+    getTile(pos.x, pos.y)
+  }
+ 
+  def generateMap() = {
+    for(x <- 0 until width; y <- 0 until height) {
+      var id = "ground"
+      var isWall = false
+    
+      if(mapTiles(y)(x) == '*') {
+        id = "wall"
+        isWall = true
+      }
+      else if (mapTiles(y)(x) == 'S') {
+        spawnerPos += Pos(x,y)
+      }
+    grid(x)(y) = new MapTile(id, x, y, isWall)
+  }
+    
+  }
+
+}
