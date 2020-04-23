9ad2e9d705ceea8ab7968b08afa0b1e84fbb299c
asulk <asulk@DESKTOP-QIN5D9B>
2020-04-23 20:25:42 +0300

    Finishing the Pos class and Object methods

--- src/Main/Pos.scala
+++ src/Main/Pos.scala
@@ -1,24 +1,60 @@
-package Main 
+package Main
 
 import scala.math._
 
-class Pos(var x: Double, var y: Double){
-  
-  override def toString() = "Pos(" + this.x + ", " + this.y + ")"
-  
-  def add(that: Pos)      = new Pos(this.x + that.x, this.y + that.y)
-  def subtract(that: Pos) = new Pos(this.x - that.x, this.y - that.y)
-  def scale(n: Double)    = new Pos(this.x * n, this.y * n)
-  def length = sqrt(x*x + y*y)
+class Pos(var x: Float, var y: Float) {
+	def +(other: Pos) = new Pos(x+other.x, y+other.y)
+  def -(other: Pos) = new Pos(x-other.x, y-other.y)
+	def *(scale: Float) = new Pos(x*scale, y*scale)
+	def *(scale: Double) = new Pos(x*scale.toFloat, y*scale.toFloat)
+	def /(scale: Float) = new Pos(x/scale, y/scale)
+	def /(scale: Double) = new Pos(x/scale.toFloat, y/scale.toFloat)
+	def length = sqrt(x*x+y*y)
+	
 
-  
-  
-  def distanceToAnother(other: Pos) = sqrt(pow(other.x - this.x, 2) + pow(other.y - this.y,2))
-  
-  def changePosTo(x: Double, y: Double) = new Pos(x,y)
-  
-  
-  
-  
+	def angle(other: Pos) : Float = {
+	  (-atan2(this.x-other.x, this.y-other.y)).toFloat
+	}
+	
+
+	def distanceToAnother(other: Pos): Float = {
+		sqrt(pow(other.x - this.x, 2) + pow(other.y - this.y , 2)).toFloat
+	}
+	
+
+	def normalize() : Unit = {
+	  if(length > 0) {
+	    var l = length.toFloat
+  	  this.x = this.x/l
+  	  this.y = this.y/l
+	  }
+	}
+
+
+	def normalized() : Pos = {
+	  var vec = Pos(this)
+	  vec.normalize()
+	  vec
+	}
+	
+
 }
 
+
+object Pos {
+  def apply(x: Int, y: Int) = {
+    new Pos(x, y)
+  }
+  
+  def apply(x: Float, y: Float) = {
+    new Pos(x, y)
+  }
+  
+  def apply(x: Double, y: Double) = {
+    new Pos(x.toFloat, y.toFloat)
+  }
+  
+  def apply(v: Pos) = {
+    new Pos(v.x, v.y)
+  }
+}
