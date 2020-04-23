3920ce04b00ce2627b88a680b750f1ad7cf0263c
asulk <asulk@DESKTOP-QIN5D9B>
2020-04-23 20:22:28 +0300

    Added some methods to EnemySpawner still need to create rounds.

--- src/Main/EnemySpawner.scala
+++ src/Main/EnemySpawner.scala
@@ -22,9 +22,20 @@ object EnemySpawner{
   var roundCount = 0
   var timeToNext = System.nanoTime() - (roundInterval*1e9)
   var round = false
+  var complete = 0l
+  var rounds = Map[Int,Round]() // add rounds
   
   
-  var waves = Map[Int,Round]() // add rounds
   
+  def isComplete() = (System.nanoTime() - complete)*1e9 < 3
   
+  def roundText() = if(round) "Round" + roundCount else "Next round in" + timeToNext
+  
+  def roundCompleteText() = {"Round" + roundCount + "complete"}
+  
+  def init() = {
+    roundCount = 0
+    timeToNext = System.nanoTime() + timeToNext*1e9
+    round = false
+  }
 }
