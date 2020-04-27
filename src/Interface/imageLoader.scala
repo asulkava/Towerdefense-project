26eecb606064ddf8971f768b3e6e4c8a16e91c89
asulk <asulk@DESKTOP-QIN5D9B>
2020-04-27 16:37:51 +0300

    imageloader

--- src/Interface/imageLoader.scala
+++ src/Interface/imageLoader.scala
@@ -1,41 +1,53 @@
 package Interface
 
+import scala.collection.mutable.Map
 import processing.core._
 import Main._
 
 object loader {
   private val names = Map[String, String] (
+      "ground" -> "ground.png",
       "wall" -> "wall.png",
-      "Pig" -> "Pig.png",
-      "Skeleton" -> "Skeleton.png",
-      "Undying" -> "Undying.png",
-      "Zombie" -> "Zombie.png",
-      "wall2" -> "wall2.png",
-      "projectile" -> "projectile.png",
+      "pig"  -> "Pig.png",
+      "skeleton"  ->"Skeleton.png",
+      "zombie"  -> "Zombie.png",
+      "tower" -> "tower.png",
+      "ammo" -> "ammo.png",
+      "back" -> "back.png",
+      "RangeTower" -> "RangeTower.png",
+      "FastTower" -> "FastTower.png",
+      "FastTowerButton" -> "FastButton.png",
+      "NormalTowerButton" -> "NormalButton.png",
+      "RangeTowerButton" -> "RangeButton.png",
+      "NormalTower" -> "NormalTower.png",
       "buttonBlur" -> "button_blur.png",
       "towerButton" -> "towerButton.png",
+      "longTowerButton" -> "longTowerButton.png",
       "playButton" -> "play.png",
-      "rightClick" -> "rightClick.png",
-      "leftClick" -> "leftClick.png",
-      "money" -> "money.png",
+      "farmback" -> "farmback.png"
   )
   
-  private val Path = "resources/imageFiles/"
+  private val imagePath = "resources/imageFiles/"
    
   def loadImage(path: String) : PImage = {
-    main.loadImage(s"${Path}${path}")
+    main.loadImage(s"${imagePath}${path}")
   }
 	
-	private val images = scala.collection.mutable.Map[String, PImage]()
+	private val images = Map[String, PImage]()
 	
+	// Load all the images
 	for(id <- names) {
 	  images(id._1) = loadImage(id._2)
 	}
 	
+	// Returns the wanted image if found
 	def get(id: String) : PImage = {
 	  var i = images.get(id)
 	  
+	  if(!i.isDefined) {
+	    throw new Exception(s"${id} is not a valid image identifier!")
+	  }
+	  
 	  i.get
 	}
-	
 }
